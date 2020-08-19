70e5b3a1e25ba7219b62eecbd3b6aea5631a83f5
lucky-fish <Lucky_fishOwO@qq.com>
2020-08-19 12:16:39 +0000

    Added import command to import whitelist from a yaml file.

--- src/main/java/cn/mcres/luckyfish/antileakaccount/bungee/command/WhiteListCommand.java
+++ src/main/java/cn/mcres/luckyfish/antileakaccount/bungee/command/WhiteListCommand.java
@@ -6,7 +6,12 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -46,10 +51,52 @@ public class WhiteListCommand extends Command {
                 break;
             }
             case "list": {
-                List<UUID> whiteList = AntiLeakAccount.getInstance().getWhiteListStorage().getWhiteList();;
+                List<UUID> whiteList = AntiLeakAccount.getInstance().getWhiteListStorage().getWhiteList();
                 for (UUID uid : whiteList) {
                     sender.sendMessage(new TextComponent(ChatColor.YELLOW + "玩家 " + ChatColor.GREEN + MojangApiHelper.getMinecraftNameByUuid(uid) + " - " + uid));
                 }
+                break;
+            }
+            case "import": {
+                if (args.length != 2) {
+                    sender.sendMessage(new TextComponent(ChatColor.GREEN + "请在插件配置目录下创建一个yaml文件"));
+                    sender.sendMessage(new TextComponent(ChatColor.GREEN + "内容格式如下："));
+                    sender.sendMessage(new TextComponent(ChatColor.GOLD + "uuids:"));
+                    sender.sendMessage(new TextComponent(ChatColor.GOLD + "  - 第一个玩家的uuid"));
+                    sender.sendMessage(new TextComponent(ChatColor.GOLD + "  - 第二个玩家的uuid"));
+                    sender.sendMessage(new TextComponent(ChatColor.GREEN + "完成后请再次输入/antileakaccount import <你创建的文件名>"));
+                    return;
+                }
+
+                File file = new File(AntiLeakAccount.getInstance().getDataFolder(), args[1]);
+                if (!file.exists()) {
+                    execute(sender, new String[]{"import"});
+                    return;
+                }
+
+                Configuration yc;
+                try {
+                    yc = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
+                } catch (IOException e) {
+                    execute(sender, new String[]{"import"});
+                    return;
+                }
+                List<String> uuidList = yc.getStringList("uuids");
+
+                int succ = 0;
+                int fail = 0;
+                for (String uid : uuidList) {
+                    try {
+                        UUID uuid = UUID.fromString(uid);
+
+                        AntiLeakAccount.getInstance().getWhiteListStorage().addWhitelistPlayer(uuid);
+                        succ ++;
+                    } catch (IllegalArgumentException e) {
+                        sender.sendMessage(new TextComponent(ChatColor.RED + "无效的uuid: " + uid));
+                        fail ++;
+                    }
+                }
+                sender.sendMessage(new TextComponent(ChatColor.GREEN + "导入完毕，导入成功 " + org.bukkit.ChatColor.GOLD + succ + org.bukkit.ChatColor.GREEN + " 个，" + "导入失败 " + org.bukkit.ChatColor.GOLD + fail + " 个"));
             }
         }
     }
---
+++ src/main/java/cn/mcres/luckyfish/antileakaccount/command/WhiteListImportCommand.java
@@ -0,0 +1,73 @@
+package cn.mcres.luckyfish.antileakaccount.command;
+
+import cn.mcres.luckyfish.antileakaccount.AntiLeakAccount;
+import cn.mcres.luckyfish.plugincommons.commands.SubCommandBase;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.util.List;
+import java.util.UUID;
+
+public class WhiteListImportCommand extends SubCommandBase {
+    @Override
+    public String getName() {
+        return "import";
+    }
+
+    @Override
+    public String getDescription() {
+        return "从文件导入白名单";
+    }
+
+    @Override
+    public String getUsage() {
+        return "import 文件名";
+    }
+
+    @Override
+    public String getPermissionRequired() {
+        return "antileakaccount.whitelist.import";
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, Command command, String[] args) {
+        if (args.length != 1) {
+            sender.sendMessage(ChatColor.GREEN + "请在插件配置目录下创建一个yaml文件");
+            sender.sendMessage(ChatColor.GREEN + "内容格式如下：");
+            sender.sendMessage(ChatColor.GOLD + "uuids:");
+            sender.sendMessage(ChatColor.GOLD + "  - 第一个玩家的uuid");
+            sender.sendMessage(ChatColor.GOLD + "  - 第二个玩家的uuid");
+            sender.sendMessage(ChatColor.GREEN + "完成后请再次输入/antileakaccount import <你创建的文件名>");
+            return true;
+        }
+
+        File file = new File(AntiLeakAccount.getInstance().getDataFolder(), args[0]);
+        if (!file.exists()) {
+            execute(sender, command, new String[0]);
+            return true;
+        }
+
+        YamlConfiguration yc = YamlConfiguration.loadConfiguration(file);
+        List<String> uuidList = yc.getStringList("uuids");
+
+        int succ = 0;
+        int fail = 0;
+        for (String uid : uuidList) {
+            try {
+                UUID uuid = UUID.fromString(uid);
+
+                AntiLeakAccount.getInstance().getWhiteListStorage().addWhitelistPlayer(uuid);
+                succ ++;
+            } catch (IllegalArgumentException e) {
+                sender.sendMessage(ChatColor.RED + "无效的uuid: " + uid);
+                fail ++;
+            }
+        }
+        sender.sendMessage(ChatColor.GREEN + "导入完毕，导入成功 " + ChatColor.GOLD + succ + ChatColor.GREEN + " 个，" + "导入失败 " + ChatColor.GOLD + fail + " 个");
+
+        return true;
+    }
+}
