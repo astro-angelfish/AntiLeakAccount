af187b46f8ca282828f3def0b5f38dbf808fb74a
lucky-fish <Lucky_fishOwO@qq.com>
2020-08-24 19:54:37 +0000

    Updated HttpServer.java

--- src/main/java/cn/mcres/luckyfish/antileakaccount/http/HttpServer.java
+++ src/main/java/cn/mcres/luckyfish/antileakaccount/http/HttpServer.java
@@ -3,6 +3,8 @@ package cn.mcres.luckyfish.antileakaccount.http;
 import cn.mcres.luckyfish.antileakaccount.AntiLeakAccount;
 import org.bukkit.Bukkit;
 import org.nanohttpd.protocols.http.NanoHTTPD;
+import org.nanohttpd.protocols.http.response.Response;
+import org.nanohttpd.protocols.http.response.Status;
 
 import java.util.UUID;
 import java.util.concurrent.Future;
@@ -10,32 +12,31 @@ import java.util.concurrent.Future;
 public class HttpServer extends NanoHTTPD {
     public HttpServer() {
         super(AntiLeakAccount.getInstance().getConfigHolder().httpdPort);
-    }
 
-    @Override
-    public Response serve(IHTTPSession session) {
-        if (!session.getUri().equals("/" + AntiLeakAccount.getInstance().getConfigHolder().verifyName)) {
-            return newFixedLengthResponse(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "你好像打错链接了> <");
-        }
+        this.setHTTPHandler(session -> {
+            if (!session.getUri().equals("/" + AntiLeakAccount.getInstance().getConfigHolder().verifyName)) {
+                return Response.newFixedLengthResponse(Status.NOT_FOUND, MIME_PLAINTEXT, "你好像打错链接了> <");
+            }
 
-        try {
-            UUID uid = UUID.fromString(session.getParms().get("uid"));
-            String sessionId = session.getParms().get("session");
-            String ip = session.getHeaders().get("http-client-ip");
-            Future<Boolean> processing = Bukkit.getScheduler().callSyncMethod(AntiLeakAccount.getInstance(), () -> AntiLeakAccount.getInstance().getVerifyManager().processRequest(ip, uid, sessionId));
-            while (!processing.isDone()) {
-                try {
-                    Thread.sleep(1);
-                } catch (Exception e) {}
+            try {
+                UUID uid = UUID.fromString(session.getParms().get("uid"));
+                String sessionId = session.getParms().get("session");
+                String ip = session.getHeaders().get("http-client-ip");
+                Future<Boolean> processing = Bukkit.getScheduler().callSyncMethod(AntiLeakAccount.getInstance(), () -> AntiLeakAccount.getInstance().getVerifyManager().processRequest(ip, uid, sessionId));
+                while (!processing.isDone()) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (Exception e) {}
+                }
+                if (processing.get()) {
+                    return Response.newFixedLengthResponse(Status.OK, MIME_PLAINTEXT, "验证完成，回到服务器玩耍吧");
+                } else {
+                    return Response.newFixedLengthResponse(Status.FORBIDDEN, MIME_PLAINTEXT, "无效的验证");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return Response.newFixedLengthResponse(Status.FORBIDDEN, MIME_PLAINTEXT, "无效的请求");
             }
-            if (processing.get()) {
-                return newFixedLengthResponse(Response.Status.OK, MIME_PLAINTEXT, "验证完成，回到服务器玩耍吧");
-            } else {
-                return newFixedLengthResponse(Response.Status.FORBIDDEN, MIME_PLAINTEXT, "无效的验证");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return newFixedLengthResponse(Response.Status.FORBIDDEN, MIME_PLAINTEXT, "无效的请求");
-        }
+        });
     }
 }
